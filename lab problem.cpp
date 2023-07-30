56aacc49bded799c9eb63b0f46d5ac7d53ae55af
Baidika <140314926+baidi104@users.noreply.github.com>
2023-07-30 15:53:12 +0600

    lab-01

---
+++ lab problem.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Hello in my University"<<endl;
+cout<<endl;
+cout<<"I'm a student in it faculty"<<endl;
+cout<<endl;
+cout<<"Welcome";
+}
+
